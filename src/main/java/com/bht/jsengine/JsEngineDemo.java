71cb379de7171e1914134f4cb96215067637ea23
binh.huynh1 <binh.huynh1@mservice.com.vn>
2021-03-01 10:26:05 +0700

    init project

---
+++ src/main/java/com/bht/jsengine/JsEngineDemo.java
@@ -0,0 +1,12 @@
+package com.bht.jsengine;
+
+/**
+ *
+ * @author binhhuynh1
+ */
+public class JsEngineDemo {
+
+    public static void main(String[] args) {
+        System.out.println("JsEngineDemo Init...");
+    }
+}
